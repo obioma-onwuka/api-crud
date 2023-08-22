d2d1d8f88f466dc2b4a18c983e3fceb2d47ad0e4
Obioma Onwuka <89937609+obioma-onwuka@users.noreply.github.com>
2023-08-22 01:35:42 +0100

    edit

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,23 +3,37 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Customer;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use Illuminate\Http\Request;
+use App\Services\V1\CustomerQuery;
 
-use App\Http\Resources\V1\CustomerResource;
-use App\Http\Resources\V1\CustomerCollection;
-
+// use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\Controller;
+
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Resources\V1\CustomerResource;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\V1\CustomerCollection;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request);
+
+        if(count($queryItems) == 0){
+
+            return new CustomerCollection(Customer::paginate());
+
+        }else{
+
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+
+        }
+
 
     }
 
