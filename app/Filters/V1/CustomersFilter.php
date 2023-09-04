11619f774bf33adef80c9ed9162f442263e5e9e8
Obioma Onwuka <89937609+obioma-onwuka@users.noreply.github.com>
2023-09-04 22:40:01 +0100

    edit

---
+++ app/Filters/V1/CustomersFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+
+class CustomersFilter extends ApiFilter {
+
+    protected $safeParams = [];
+
+    protected $columnMap = [];
+
+    protected $operatorMap = [];
+
+    public function transform(Request $request){
+
+        $eloQuery = [];
+
+        foreach($this->safeParams as $param => $operators){
+
+            $query = $request->query($param);
+
+            if(!isset($query)){
+
+                continue;
+
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach($operators as $operator){
+
+                if(isset($query[$operator])){
+
+                    $eloQuery[] =[$column, $this->operatorMap[$operator], $query[$operator]];
+
+                }
+
+            }
+
+        }
+
+        return $eloQuery;
+
+    }
+
+}
